b211a12625c6c768b11a7be88e590f2d5390ce6d
golmem <toloumpro@gmail.com>
2022-07-18 18:06:01 +0000

    lien actif

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,11 +8,15 @@ const Navigation = () => {
 			<ul>
 				{/* le to sert de href  et la balise navlink sert de a*/}
 				{/* le premier navlink mene a la page Home*/}
-				<NavLink to="/">
+				{/* stylisation du lien actif avec nav.isActive*/}
+				<NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>
 					<li>Acceuil</li>
 				</NavLink>
 				{/*le second navlink mene a la page About*/}
-				<NavLink to="/about">
+				<NavLink
+					to="/about"
+					className={(nav) => (nav.isActive ? "nav-active" : "")}
+				>
 					<li>A Propos</li>
 				</NavLink>
 			</ul>
