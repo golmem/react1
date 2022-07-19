ee2d417274cbc2399b94172a4fd85106f8097894
golmem <toloumpro@gmail.com>
2022-07-19 11:37:59 +0000

    utilisation de useEffect et de axios get avec l'api restcountries

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -2,9 +2,16 @@ import React from "react";
 
 const Countries = () => {
 	return (
-		<div className="countries">
-			<p>Countries</p>
-		</div>
+		// le useEffect est appelé quand le composant est monté c'est a dire appelé
+		useEffect(() => {
+			//utilisation de axios pour recuperer les données de l'api restcountries
+			axios.get("https://restcountries.com/v3.1/all");
+		}, []),
+		(
+			<div className="countries">
+				<p>Countries</p>
+			</div>
+		)
 	);
 };
 
